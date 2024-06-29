245800d5c7f80b59fca9eb5e57c0cba073927c42
yashvisal <yashvisal@gmail.com>
2024-06-28 22:50:55 -0700

    empty page basic UI

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const DocumentsPage = () => {
+    const { user } = useUser();
+
     return ( 
-        <div>
-            Documents
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            {/* TODO: get a light mode and dark mode image, upload to public folder, replace names and uncomment */}
+            {/* <Image
+                src="image-file-name.png"
+                height={300}
+                width={300}
+                alt="Empty"
+                className="dark:hidden"
+            />
+            <Image
+                src="image-file-name-dark.png"
+                height={300}
+                width={300}
+                alt="Empty"
+                className="hidden dark:block"
+            /> */}
+            <h2 className="text-lg font-medium">
+                Welcome to {user?.firstName}&apos;s Schol
+            </h2>
+            <Button>
+                <PlusCircle className="w-4 h-4 mr-2" />
+                Add a new class!
+            </Button>
         </div>
     );
 }
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
     const pathname = usePathname();
@@ -110,7 +111,7 @@ export const Navigation = () => {
                     <ChevronsLeft className="w-6 h-6" />
                 </div>
                 <div>
-                    <p>Action Items</p>
+                    <UserItem />
                 </div>
                 <div className="mt-4">
                     <p>Documents</p>
