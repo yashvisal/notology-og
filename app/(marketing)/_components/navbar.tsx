eee4b023e32750f85d3a4ee22a69b234d377c111
yashvisal <yashvisal@gmail.com>
2024-06-28 19:36:14 -0700

    sign in button + clerk auth

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,10 +1,48 @@
+"use client";
+
 import { ModeToggle } from "@/components/mode-toggle";
+import { Spinner } from "@/components/spinner";
+import { Button } from "@/components/ui/button";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import Link from "next/link";
 
 const Navbar = () => {
+    const { isAuthenticated, isLoading} = useConvexAuth();
+
     return (
         <div className="z-50 bg-background fixed top-0 flex items-center w-full p-6">
             Schol
             <div className="justify-end w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <Spinner />
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button  variant="ghost" size="sm">
+                                Log in
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm">
+                                Get Schol
+                            </Button>
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">
+                                Enter Schol
+                            </Link>
+                        </Button>
+                        <UserButton 
+                            afterSignOutUrl="/"
+                        />
+                    </>
+                )}
                 <ModeToggle />
             </div>
         </div>       
