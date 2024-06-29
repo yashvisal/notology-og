e8bd838f927906b1bbed8a38105e5fdf87659cff
yashvisal <yashvisal@gmail.com>
2024-06-29 15:56:10 -0700

    doc create function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -28,6 +29,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="schol-theme"
           >
+            <Toaster position="bottom-center"/>
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
