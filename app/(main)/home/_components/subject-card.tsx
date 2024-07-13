215c862ac3ff615b7e60837cf42b384fe2b87866
yashvisal <yashvisal@gmail.com>
2024-07-13 02:32:37 -0700

    subject page routing

--- app/(main)/home/_components/subject-card.tsx
+++ app/(main)/home/_components/subject-card.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Book, Plus } from "lucide-react";
 import { CreateSubjectDialog } from "./new-subject-dialog";
+import { useRouter } from "next/navigation";
 
 interface SubjectCardProps {
     subject?: {
@@ -11,11 +12,12 @@ interface SubjectCardProps {
 }
 
 const SubjectCard = ({ subject, isAddNew }: SubjectCardProps) => {
+    const router = useRouter();
+
     const renderAddNewCard = () => (
         <CreateSubjectDialog>
             <Card
                 className="w-[150px] rounded-xl transition cursor-pointer flex-shrink-0 border border-gray-200 hover:border-gray-300 hover:shadow-sm duration-150 ease-in-out"
-                onClick={() => {}}
             >
                 <CardContent className="p-4 flex flex-col items-start justify-between h-full font-medium text-primary/70">
                     <Plus className="h-4 w-4" />
@@ -28,7 +30,7 @@ const SubjectCard = ({ subject, isAddNew }: SubjectCardProps) => {
     const renderSubjectCard = () => (
         <Card
             className="w-[150px] rounded-xl transition cursor-pointer flex-shrink-0 border border-gray-200 hover:border-gray-300 hover:shadow-sm duration-150 ease-in-out"
-            onClick={() => {}}
+            onClick={() => router.push(`/subjects/${subject!._id}`)}
         >
             <CardContent className="p-4 flex flex-col gap-12 items-start justify-between h-full font-medium text-primary/70">
                 <Book className="h-4 w-4" />
