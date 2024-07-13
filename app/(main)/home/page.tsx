457179d6d62a5db694183c648d1fd3afec973e03
yashvisal <yashvisal@gmail.com>
2024-07-13 02:21:43 -0700

    container height change

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/clerk-react";
 import SubjectSummary from "./_components/subject-summary";
 import StudyStreak from "./_components/study-streak";
 
 export default function Component() {
     const { user } = useUser();
-    const subjects = useQuery(api.subjects.getSubjects);
 
     const getGreeting = () => {
         const hour = new Date().getHours();
@@ -20,7 +17,7 @@ export default function Component() {
     return (
         <div className="flex flex-col h-full transition-all ease-in-out w-full">
             <div className="w-full h-full relative overflow-hidden">
-                <div className="overflow-y-auto transition-all ease-in-out duration-200 h-[calc(100dvh-54px)] scrollbar-hide w-full">
+                <div className="overflow-y-auto transition-all ease-in-out duration-200 scrollbar-hide w-full">
                     <div className="mx-auto max-w-3xl mt-12 px-4">
                         <h1 className="text-3xl font-semibold text-center mb-8">
                             {getGreeting()}, {user?.firstName || 'Student'}!
