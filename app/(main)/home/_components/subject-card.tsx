3261989d70ee5974eadc77c70676c05fe9a9e8f7
yashvisal <yashvisal@gmail.com>
2024-07-13 01:19:12 -0700

    subject handling

--- app/(main)/home/_components/subject-card.tsx
+++ app/(main)/home/_components/subject-card.tsx
@@ -1,26 +1,51 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { FileText } from "lucide-react";
+import { Book, Plus } from "lucide-react";
+import { useState } from "react";
+import { NewSubjectDialog } from "./new-subject-dialogue";
 
 interface SubjectCardProps {
-    subject: {
+    subject?: {
         _id: string;
         name: string;
         isArchived: boolean;
         fileId?: string;
     };
+    isAddNew?: boolean;
 }
 
-const SubjectCard = ({ subject }: SubjectCardProps) => {
-    return (
-        <Card className="w-[150px] hover:bg-gray-50 transition cursor-pointer">
-            <CardContent className="p-4">
-                <div className="flex items-center mb-2">
-                    <FileText className="mr-2 h-4 w-4" />
-                    <div className="font-medium truncate">{subject.name}</div>
-                </div>
-                {/* Add more details here if needed */}
+const SubjectCard = ({ subject, isAddNew }: SubjectCardProps) => {
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const handleAddNew = () => setIsDialogOpen(true);
+
+    const renderAddNewCard = () => (
+        <Card 
+            className="w-[150px] rounded-xl transition cursor-pointer flex-shrink-0 border border-gray-200 hover:border-gray-300 hover:shadow-sm duration-150 ease-in-out"
+            onClick={handleAddNew}
+        >
+            <CardContent className="p-4 flex flex-col items-start justify-between h-full space-y-2">
+                <Plus className="h-4 w-4" />
+                <div className="text-sm font-medium">New Subject</div>
             </CardContent>
         </Card>
+    );
+
+    const renderSubjectCard = () => (
+        <Card className="w-[150px] rounded-xl transition cursor-pointer flex-shrink-0 border border-gray-200 hover:border-gray-300 hover:shadow-sm duration-150 ease-in-out">
+            <CardContent className="p-4 flex flex-col gap-4 items-start justify-between h-full space-y-2">
+                <Book className="h-4 w-4 text-primary" />
+                <div className="text-sm font-medium truncate">{subject!.name}</div>
+            </CardContent>
+        </Card>
+    );
+    
+    return (
+        <>
+            {isAddNew ? renderAddNewCard() : subject && renderSubjectCard()}
+            <NewSubjectDialog
+                open={isDialogOpen} 
+                onOpenChange={setIsDialogOpen}
+            />
+        </>
     );
 };
 
