0fab84ba3ab02488f3c97bc5f2e8cfa6d50ab520
yashvisal <yashvisal@gmail.com>
2024-07-15 18:16:45 -0700

    landing page

---
+++ components/landing/hero-section.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { BorderBeam } from "@/components/magicui/border-beam";
+import TextShimmer from "@/components/magicui/text-shimmer";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
+
+import { PopupButton } from "@typeform/embed-react";
+
+export default function HeroSection() {
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true, margin: "-100px" });
+
+  return (
+    <section
+      id="hero"
+      className="relative mx-auto mt-24 max-w-[80rem] px-6 text-center md:px-8"
+    >
+      {/* <div className="backdrop-filter-[12px] inline-flex h-7 items-center justify-between rounded-full border border-white/5 bg-white/10 px-3 text-xs text-white dark:text-black transition-all ease-in hover:cursor-pointer hover:bg-white/20 group gap-1 translate-y-[-1rem] animate-fade-in opacity-0">
+        <TextShimmer className="inline-flex items-center justify-center">
+          <span>✨ Introducing Notology!</span>{" "}
+          <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+        </TextShimmer>
+      </div> */}
+      <h1 className="bg-gradient-to-br dark:from-white from-black from-30% dark:to-white/40 to-black/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-5xl md:text-6xl lg:text-7xl translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
+        Study smarter,
+        <br className="hidden md:block" />not harder
+      </h1>
+      <p className="mb-8 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
+        Beautifully designed, animated components and templates built with
+        <br className="hidden md:block" /> Tailwind CSS, React, and Framer
+        Motion.
+      </p>
+
+      <div className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:600ms]">
+        <PopupButton
+          id="fPcdQIrU"
+          className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
+        >
+          Join the waitlist!
+        </PopupButton>
+      </div>
+
+      <div
+        ref={ref}
+        className="relative mt-[8rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
+      >
+        <div
+          className={`rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)] ${
+            inView ? "before:animate-image-glow" : ""
+          }`}
+        >
+          <BorderBeam
+            size={200}
+            duration={12}
+            delay={11}
+            colorFrom="var(--color-one)"
+            colorTo="var(--color-two)"
+          />
+
+          <img
+            src="/hero-dark.png"
+            alt="Hero Image"
+            className="hidden relative w-full h-full rounded-[inherit] border object-contain dark:block"
+          />
+          <img
+            src="/hero-light.png"
+            alt="Hero Image"
+            className="block relative w-full h-full  rounded-[inherit] border object-contain dark:hidden"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
