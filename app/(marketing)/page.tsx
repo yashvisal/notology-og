3dc0f13d6da6371ffb453e910fc74af5a55e9d71
yashvisal <yashvisal@gmail.com>
2024-07-17 23:58:34 -0700

    landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,19 +1,30 @@
-import Particles from "@/components/magicui/particles";
-import Heading from "./_components/heading";
 import HeroSection from "./_components/hero-section";
+import { GridPattern } from "@/components/magicui/animated-grid-pattern";
 
 export default async function Page() {
-    return (
-        <>
-            <HeroSection />
-            <Particles
-                className="absolute inset-0 -z-10"
-                quantity={50}
-                ease={70}
-                size={0.05}
-                staticity={40}
-                color={"#000000"}
-            />
-        </>
-    );
+  return (
+    <>
+      <div className="fixed inset-0 -z-10 blur-[5px]">
+        <GridPattern
+          maxOpacity={0.05}
+          width={65}
+          height={65}
+          numSquares={150}
+        />
+      </div>
+      <HeroSection />
+      {/* <ClientSection />
+      <SphereMask />
+      <PricingSection />
+      <CallToActionSection />
+      <Particles
+        className="absolute inset-0 -z-10"
+        quantity={50}
+        ease={70}
+        size={0.05}
+        staticity={40}
+        color={"#000000"}
+      /> */}
+    </>
+  );
 }
