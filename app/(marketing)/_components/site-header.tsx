7545094e32b13080a8dcc4de071294aa6b3c3ce3
yashvisal <yashvisal@gmail.com>
2024-07-18 00:05:40 -0700

    landing page header

--- app/(marketing)/_components/site-header.tsx
+++ app/(marketing)/_components/site-header.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export function SiteHeader() {
   return (
     <>
-      <header className="fixed left-0 top-0 z-50 w-full border-b backdrop-blur-[12px]">
-        <div className="container flex h-[3.5rem] items-center justify-between">
-          <Link className="text-medium flex items-center" href="/">
-            Notology
+      <header className="fixed left-0 top-0 z-50 w-full translate-y-[-1rem] animate-fade-in opacity-0 bg-white border-b [--animation-delay:600ms]">
+        <div className="container flex h-[3.5rem] px-18 items-center justify-between">
+          <Link className="text-xl font-bold flex items-center" href="/">
+            notology.
           </Link>
-          <div className="ml-auto flex h-full items-center">
+          {/* <div className="ml-auto flex h-full items-center">
             <Link className="mr-6 text-sm" href="/signin">
               Log in
             </Link>
@@ -25,7 +23,7 @@ export function SiteHeader() {
             >
               Sign up
             </Link>
-          </div>
+          </div> */}
         </div>
       </header>
     </>
