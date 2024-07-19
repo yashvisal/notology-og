08b7095397b23ac05150da64287a5d1fbd778618
yashvisal <yashvisal@gmail.com>
2024-07-18 17:39:37 -0700

    toolbar styling

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -53,12 +53,12 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
                     value={value}
                     onChange={(e) => onInput(e.target.value)}
                     onKeyDown={onKeyDown}
-                    className="pb-[11.5px] text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+                    className="pb-[11.5px] text-4xl bg-transparent font-semibold break-words outline-none text-[#000000] dark:text-[#CFCFCF] resize-none"
                     />
                 ) : (
                     <div
                     onClick={enableInput}
-                    className="pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF]"
+                    className="pb-[11.5px] text-4xl font-semibold break-words outline-none text-[#000000] dark:text-[#CFCFCF]"
                     >
                         {initialData.title}
                     </div>
