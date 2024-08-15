c9eff054da1d6e2c0a16f2fb97008330c3b4ad28
yashvisal <yashvisal@gmail.com>
2024-08-14 17:31:39 -0700

    noto icon update

--- components/message.tsx
+++ components/message.tsx
@@ -1,6 +1,7 @@
 import Markdown from "react-markdown";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { useUser } from "@clerk/clerk-react";
+import { Pencil, Sparkles, WandSparkles } from "lucide-react";
 
 export interface MessageTextProps {
   content: string;
@@ -11,7 +12,9 @@ export function AIMessageText(props: MessageTextProps) {
     <div className="flex flex-col">
       <div className="flex items-center mb-2 mt-2">
         <div className="relative h-3 w-3 mr-2">
-          <Avatar className="h-3 w-3 bg-gray-300"/>
+          {/* <Avatar className="h-3 w-3 bg-gray-300"/> */}
+          {/* <Pencil className="h-3 w-3"/> */}
+          <Sparkles className="h-3 w-3" strokeWidth={1.75}/>
         </div>
         <span className="text-xs text-black">NOTO</span>
       </div>
